18b6bea4a55ae5a96e118c641bd91a48466254f4
cristina <cristina@movilidads-imac.local>
2017-02-05 00:40:36 +0100

    Initial Commit

---
+++ DistanceCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DistanceCalculator
+//
+//  Created by cristina on 5/2/17.
+//  Copyright © 2017 crisbarreiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DistanceCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DistanceCalculator
+//
+//  Created by cristina on 5/2/17.
+//  Copyright © 2017 crisbarreiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
