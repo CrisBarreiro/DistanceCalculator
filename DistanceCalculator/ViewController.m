1637cc85c66bd5a94594511f749926b518f5e7df
Cristina S. Barreiro <cristina.sbarreiro@gmail.com>
2017-02-12 00:59:06 +0100

    Create outlets

--- DistanceCalculator/ViewController.m
+++ DistanceCalculator/ViewController.m
@@ -9,6 +9,18 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *startingLocation;
+
+@property (weak, nonatomic) IBOutlet UITextField *endLocationA;
+@property (weak, nonatomic) IBOutlet UILabel *distanceA;
+
+@property (weak, nonatomic) IBOutlet UITextField *endLocationB;
+@property (weak, nonatomic) IBOutlet UILabel *distanceB;
+
+@property (weak, nonatomic) IBOutlet UITextField *endLocationC;
+@property (weak, nonatomic) IBOutlet UILabel *distanceC;
+
+@property (weak, nonatomic) IBOutlet UIButton *calculateButton;
 
 @end
 
